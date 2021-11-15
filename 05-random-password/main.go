6c638c3fe3287fd284810687a179588c35f491d6
Jackson Atkins <realugbun@ugbun.com>
2021-11-15 17:10:31 -0500

    added flags to akilans mini project

--- 05-random-password/main.go
+++ 05-random-password/main.go
@@ -1,27 +1,47 @@
 package main
 
 import (
-	"bufio"
+	"flag"
 	"fmt"
 	"math/rand"
 	"os"
-	"strconv"
 	"time"
 )
 
 // define all the global variables
-var (
+const (
 	lowerCharSet          = "abcdefghijklmnopqrstuvwxyz"
 	upperCharSet          = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 	specialCharSet string = "!@#$%^&*()-"
 	numberCharSet         = "123567890"
-	minSpecialChar        = 2
-	minUpperChar          = 2
-	minNumberChar         = 2
-	passwordLength        = 10
 )
 
 func main() {
+
+	var (
+		passwordLength    int
+		numberOfPasswords int
+		minSpecialChar    int
+		minUpperChar      int
+		minNumberChar     int
+	)
+
+	// Set the flags. The first value is the flag, the second is the default value, the third is the help message. Try go run main.go -h
+	pwl := flag.Int("length", 10, "The length of the desired password")
+	npw := flag.Int("count", 1, "The number of desired passwords")
+	s := flag.Int("min-special", 2, "The minimum number of special characters")
+	u := flag.Int("min-upper", 2, "The minimum number of uppercase characters")
+	n := flag.Int("min-number", 2, "The minimum number of numbers")
+
+	// Tell the application to read the values
+	flag.Parse()
+
+	// Set the variables to be equal to what was provided via flags. Note: flags are pointers
+	passwordLength = *pwl
+	numberOfPasswords = *npw
+	minSpecialChar = *s
+	minUpperChar = *u
+	minNumberChar = *n
 
 	//check if the password length matches the criteria
 
@@ -32,29 +52,17 @@ func main() {
 		os.Exit(1)
 	}
 
-	// Get the user input - target folder needs to be organized
-	scanner := bufio.NewScanner(os.Stdin)
-	fmt.Printf("How many passwords you want to generate? - ")
-	scanner.Scan()
-
-	numberOfPasswords, err := strconv.Atoi(scanner.Text())
-
-	if err != nil {
-		fmt.Println("Please provide correct value for number of passwords")
-		os.Exit(1)
-	}
-
 	// it generate random number e
-	rand.Seed(time.Now().Unix())
+	rand.Seed(time.Now().UnixNano())
 
 	for i := 0; i < numberOfPasswords; i++ {
-		password := generatePassword()
+		password := generatePassword(passwordLength, minSpecialChar, minUpperChar, minNumberChar)
 		fmt.Printf("Password %v is %v \n", i+1, password)
 	}
 
 }
 
-func generatePassword() string {
+func generatePassword(passwordLength int, minSpecialChar int, minUpperChar int, minNumberChar int) string {
 
 	// declare empty password variable
 	password := ""
